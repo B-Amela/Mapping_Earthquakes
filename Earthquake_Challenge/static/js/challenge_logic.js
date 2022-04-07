843bd2d5cd33b4aa839e8c52cf7bdb998b0adb2b
Amela Begic <begic.amela@mayo.edu>
2022-04-07 17:00:16 -0500

    final

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -1,6 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
-var API_KEY ="pk.eyJ1IjoiYmFtZWxhIiwiYSI6ImNsMTAwYXFsbTByMjYzZnBheDFmaTdlcXIifQ.cBUJBGlZG-fK1KHFP6OuXQ"
+var API_KEY ="pk.eyJ1IjoiYmFtZWxhIiwiYSI6ImNsMTAwYXFsbTByMjYzZnBheDFmaTdlcXIifQ.cBUJBGlZG-fK1KHFP6OuXQ";
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
